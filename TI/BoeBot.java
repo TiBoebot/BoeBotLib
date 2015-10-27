5f41f5ffd220089472c45bd98aa449ae3798f614
Johan Talboom <borfje@gmail.com>
2015-10-27 18:03:26 +0100

    Added freqOut method

--- TI/BoeBot.java
+++ TI/BoeBot.java
@@ -8,8 +8,8 @@ package TI;
 public class BoeBot
 {  
     /** Mapping of BoeBot Pin number to Raspberry Pi Pin number */
-	static int[] pinMap = { 17, 27, 22, 5, 6, 13, 19, 26, 18, 23, 24, 25, 12, 16, 20, 21};
-	
+    static int[] pinMap = { 17, 27, 22, 5, 6, 13, 19, 26, 18, 23, 24, 25, 12, 16, 20, 21};
+    
     
     /**
      * Will put a pin in output mode, and set the state high if value is true, and low if value is false
@@ -56,7 +56,7 @@ public class BoeBot
      */
     public static void wait(int seconds, int micros)
     {
-    	PiGpio.delay(seconds, micros);
+        PiGpio.delay(seconds, micros);
     }
     /**
      * Waits for a specific time
@@ -65,19 +65,19 @@ public class BoeBot
      */
     public static void uwait(int micros)
     {
-    	PiGpio.delay(0, micros);
+        PiGpio.delay(0, micros);
     }
     
    /**
     * Waits for a pulse on a pin
-	* @param pin the BoeBot pin number
-	* @param value the state to wait for. true to wait for a high pulse, false to wait for a low pulse
-	* @param timeout the timeout to wait for, in milliseconds
-	* @return Returns the length of the measured pulse, -1 if there was a timeout, and 0 if there was a timeout during the pulse
+    * @param pin the BoeBot pin number
+    * @param value the state to wait for. true to wait for a high pulse, false to wait for a low pulse
+    * @param timeout the timeout to wait for, in milliseconds
+    * @return Returns the length of the measured pulse, -1 if there was a timeout, and 0 if there was a timeout during the pulse
     */
     public static int pulseIn(int pin, boolean value, int timeout)
     {
-    	return PiGpio.pulseIn(pinMap[pin], value, timeout);
+        return PiGpio.pulseIn(pinMap[pin], value, timeout);
     }
     
 
@@ -98,5 +98,26 @@ public class BoeBot
     public static int dataRead(int slot)
     {
         return 0;
+    }
+    
+    /**
+     * Writes a frequency to a pin, for a certain amount of time
+     * @param pin the BoeBot pin number
+     * @param frequency the frequency to write, in Hz
+     * @param time the amount of time to write, in milliseconds
+     */
+    public static void freqOut(int pin, int frequency, int time)
+    {
+        if(frequency < 0 || time < 0)
+            return;
+
+        long endTime = System.currentTimeMillis() + time;
+        boolean value = false;
+        while(endTime > System.currentTimeMillis())
+        {
+            digitalWrite(pin, value);
+            value = !value;
+            PiGpio.delay(0, 1000000 / (frequency*2));
+        }
     }
 }
---
+++ genjar.bat
@@ -0,0 +1,4 @@
+SET PATH=%PATH%;"c:\Program Files\Java\jdk1.8.0_60\bin"
+javac TI/*.java
+jar -cf BoeBotLib.jar TI/*.class
+pause
